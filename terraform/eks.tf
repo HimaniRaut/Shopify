1395f3daf55f6b33eb276bbb727f24d5f457cfd9
Himani Raut <85188284+HimaniRaut@users.noreply.github.com>
2026-07-05 02:58:50 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -26,7 +26,7 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
 
-    instance_types = ["t2.large"]
+    instance_types = ["m7i-flex.large"]
 
     attach_cluster_primary_security_group = true
 
@@ -35,19 +35,19 @@ module "eks" {
 
   eks_managed_node_groups = {
 
-    qualibytes-demo-ng = {
+    shopify-demo-ng = {
       min_size     = 2
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t2.large"]
+      instance_types = ["m7i-flex.large"]
       capacity_type  = "SPOT"
 
       disk_size = 35 
       use_custom_launch_template = false  # Important to apply disk size!
 
       tags = {
-        Name = "qualibytes-demo-ng"
+        Name = "shopify-demo-ng"
         Environment = "dev"
         ExtraTag = "e-commerce-app"
       }

0025fbb3ef5bbac002c8bcbef24e3423a366e319
Himani Raut <85188284+HimaniRaut@users.noreply.github.com>
2026-07-06 03:23:47 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,7 +13,7 @@ data "aws_ami" "os_image" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("Shopify-key.pub")
+  public_key = file("shopify-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
